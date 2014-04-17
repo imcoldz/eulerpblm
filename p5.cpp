b2e49f99132ee94d2f28549fe983e98afebdfa39
imcoldz <daneozxy@gmail.com>
2014-04-17 11:53:48 -0700

    Update p5.cpp

--- p5.cpp
+++ p5.cpp
@@ -16,7 +16,9 @@ bool isPrime(int val){
 	if(val<2) return false;
 	if(primevec.empty()) return true;
 	else{
-		for(int i=(int)primevec.size()-1; i>=0; --i){
+		int highbound=sqrt(val)+1;
+		for(int i=0; i<primevec.size(); ++i){
+			if(primevec[i]>highbound) break;
 			if(val%primevec[i]==0) return false;
 		}
 		return true;
@@ -27,36 +29,15 @@ int main() {
 	// your code goes here
 	int k=20; //assert(k>=2);
 	int target=1;
-	unordered_map<int,int> compositionMap;
 	for(int i=2; i<=k; ++i){
 		//cout<<"judging value: "<<i<<endl;
 		if(isPrime(i)){
 			//cout<<"it is a prime."<<endl;
-			target*=i;
+			int exp=log(k)/log(i);
+			target*=pow(i, exp);
 			//cout<<"target updated to: "<<target<<endl;
 			primevec.push_back(i);
-		    compositionMap[i]=1;
-		}else{
-			int tmp=i;
-			//cout<<"it is a non-prime."<<endl;
-			//decompose each non-prime value and check whether you need to multiply a factor again
-			for(int j=0; j<(int)primevec.size(); ++j){
-				int cnt=0;
-				int prime=primevec[j];
-				while(i>1 && i%prime==0){
-					i/=prime;
-					cnt++;
-				}
-				//cout<<"has "<<cnt<<" number of factor: "<<primevec[j]<<endl;
-				//cout<<"target has already included "<<compositionMap[prime]<<" number of this factor"<<endl;
-				if(cnt>compositionMap[prime]){
-					target*=pow(primevec[j], cnt-compositionMap[prime]);
-					//cout<<"target updated to: "<<target<<endl;
-					compositionMap[prime]=cnt;
-				}
-			}
-			i=tmp;
-		} 
+		}
 	}
 	cout<<target<<endl;
 	return 0;
